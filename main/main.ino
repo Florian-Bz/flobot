eeae5b7c0c5106fb0216f2cb9d222214d04975de
“Florian-Bz” <florian.dallago@gmail.com>
2024-01-02 13:31:11 +0100

    use references

--- main/main.ino
+++ main/main.ino
@@ -3,28 +3,34 @@
 // Link lengths
 int l1 = 200, l2 = 200; // [mm]
 
+// Joint angles (base,shoulder,elbow)
+int joint_angles[3];
+
+// EEF coordinates (x,y,z)
+int eef_coords[3];
+
+
 /**
 theta_b (base)
 theta_s (shoulder)
 theta_e (elbow)
-theta_w (wrist)
 **/
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println("hello");
+  delay(1000); // [ms]
 }
 
-int DK(int theta_s, int theta_e) {
+void DK(int &theta_1, int &theta_2, int &x, int &y) {
   // Direct kinematics (input: joint angles | output: eef coords)
-  int x = l1*sin(theta_s) + l2*sin(theta_s+theta_e);
-  int y = l1*cos(theta_s) + l2*cos(theta_s+theta_e);
-  return x, y;
+  x = l1*sin(theta_1) + l2*sin(theta_1+theta_2);
+  y = l1*cos(theta_1) + l2*cos(theta_1+theta_2);
 }
 
 void IK() {

41357639b15151216ae7fd94f6cb69172cabd591
“Florian-Bz” <florian.dallago@gmail.com>
2024-01-02 18:02:54 +0100

    inverse kinematics

--- main/main.ino
+++ main/main.ino
@@ -38,13 +38,25 @@ void loop() {
   delay(1000); // [ms]
 }
 
-void DK(int theta_1, int theta_2, int &x, int &y) {
+void DK(float theta_s, float theta_e, float &x, float &y) {
   // Direct kinematics (input: joint angles | output: eef coords)
-  x = l1*sin(theta_1) + l2*sin(theta_1+theta_2);
-  y = l1*cos(theta_1) + l2*cos(theta_1+theta_2);
+  x = l1*sin(theta_s) + l2*sin(theta_s+theta_e);
+  y = l1*cos(theta_s) + l2*cos(theta_s+theta_e);
 }
 
-void IK() {
+void IK(float x, float y, float &theta_s, float &theta_e) {
   // Inverse kinematics (input: eef coords | output: joint angles)
+  float radius = sqrt(x*x + y*y); // distance from the origin to the eef
+  float theta = atan2(x,y); // angle from x-axis to the radius
+  float cos_beta = (radius*radius - l1*l2 - l2-l2) / (-2 * l1 * l2)  // internal angle btw links
+
+  float beta;
+  if (cos_beta < -1.0) {
+    beta = PI; }
+  else if (cos_beta > 1.0) {
+    beta = 0.0; }
+  else {
+    beta = acos(cos_beta); }
+
 
 }

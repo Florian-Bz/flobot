4edac7119b7913575159c3f251dbe08c6c88f9dd
“Florian-Bz” <florian.dallago@gmail.com>
2024-05-26 16:21:32 +0200

    code cleaning

--- tryServo/tryServo.ino
+++ tryServo/tryServo.ino
@@ -25,9 +25,8 @@ void setup()
   // call function to adjust position
 
   // move to home pos
-  joint0.write(theta0);
-  joint1.write(theta1);
-  joint2.write(theta2);
+  delay(1000);
+  home_pos(0, 15);
 
   delay(1000);
   move_to(joint0, 180, 15); // joint(0,1,2), pos(0-180), speed(0-30)
@@ -49,34 +48,29 @@ void loop()
 
 void move_to(Servo &joint, int position, int speed)
 {
-
-  // if (motorID = 0) {joint = joint0;}
-  // if (motorID = 1) {joint = joint1;}
-  // if (motorID = 2) {joint = joint2;}
-
   int currentPos = joint.read(); // read motor angle [deg]
   speed = map(speed, 0, 30, 30, 0); // map(value, fromCurrentLow(speed), fromCurrentHigh(speed), toTargetLow(time), toTargetHigh(time))
   if (position > currentPos) {
     for (; currentPos <= position; currentPos += 1) {
       joint.write(currentPos);
-      //theta0 = currentPos;
       delay(speed);
     }
   }
   else {
     for (; currentPos >= position; currentPos -= 1) {
       joint.write(currentPos);
-      //theta0 = currentPos;
       delay(speed);
     }
   }
 }
 
 
-// void home_pos()
-// {
-//   move_to()
-// }
+void home_pos(int homing_pos, int homing_speed)
+{
+  move_to(joint0,homing_pos,homing_speed);
+  // move_to(joint1,homing_pos,homing_speed);
+  // move_to(joint2,homing_pos,homing_speed);
+}
 
 
 // int read_angle(join)

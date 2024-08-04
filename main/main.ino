5f13f2c9fc6684b4146787cce6d7e631a4cfdd08
“Florian-Bz” <florian.dallago@gmail.com>
2024-08-04 09:58:16 +0200

    start implementing potentiometer

--- main/main.ino
+++ main/main.ino
@@ -15,10 +15,12 @@ int theta0 = 90; // Base
 int theta1 = 90; // Shoulder
 int theta2 = 90; // Elbow
 
+// defining potentiometer pins
 int pin0Pot = A0; // Base potentiometer pin
 int pin1Pot = A1; // Shoulder potentiometer pin
 int pin2Pot = A2; // Elbow potentiometer pin
 
+// defininig potentiometer read values
 float pot0Val; // Base potentiometer value
 float pot1Val; // Shoulder potentiometer value
 float pot2Val; // Elbow potentiometer value
@@ -36,9 +38,9 @@ void setup() {
   delay(1500);
 
   // call function to adjust position
-  move_to(joint0, 135, 15); // joint(0,1,2), pos(0-180), speed(0-30)
-  move_to(joint1, 135, 15); // joint(0,1,2), pos(0-180), speed(0-30)
-  move_to(joint2, 135, 15); // joint(0,1,2), pos(0-180), speed(0-30)
+  // move_to(joint0, 135, 15); // joint(0,1,2), pos(0-180), speed(0-30)
+  // move_to(joint1, 135, 15); // joint(0,1,2), pos(0-180), speed(0-30)
+  // move_to(joint2, 135, 15); // joint(0,1,2), pos(0-180), speed(0-30)
 
 }
 
@@ -46,6 +48,21 @@ void setup() {
 void loop() { 
   // joint0.write(0); //command to rotate the servo to the specified angle (range: 0-180)
   // delay(2000);
+
+  // read potentiometer
+  // write to motor
+  
+}
+
+
+/**
+Servo motors jump to 90 deg at startup by default.
+To change this angel do a .write() with the desired angle before .attach()
+*/
+void home_pos(int homing_pos) {
+  joint0.write(homing_pos);
+  joint1.write(homing_pos);
+  joint2.write(homing_pos);
 }
 
 
@@ -72,20 +89,11 @@ void move_to(Servo &joint, int position, int speed) {
 
 
 /**
-Servo motors jump to 90 deg at startup by default.
-To change this angel do a .write() with the desired angle before .attach()
-*/
-void home_pos(int homing_pos) {
-  joint0.write(homing_pos);
-  joint1.write(homing_pos);
-  joint2.write(homing_pos);
-}
-
-
-/**
 Convert potentiometer value to motor angle
 */
-float knobToAngle(float potVal) {
+float knobToAngle(int motorID) {
+  
+  float potVal = analogRead(pinPot);
   float motorAngle = (180./1023.)*potVal;
   return motorAngle;
 }
